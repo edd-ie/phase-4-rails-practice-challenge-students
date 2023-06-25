935cdb9130ee4a521e292d09f1e50b660ebfdb4d
edd-ie <ogendoeddie@gmail.com>
2023-06-25 14:14:22 +0300

    Instructor controller functions

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,51 @@
+class InstructorsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with:  :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+
+    def index
+        instructors = Instructor.all
+        render json: instructors, except: [:created_at, :updated_at], status: :ok
+    end
+
+    def show
+        instructor = finder
+        render json: instructor, except: [:created_at, :updated_at], status: :ok
+    end
+
+    def create
+        new_ instructor = Instructor.create!(valid_params)
+        render json: new_instructor, except: [:created_at, :updated_at], status: :created
+    end
+
+    def update
+        instructor = finder
+        instructor.update!(valid_params)
+        render json: instructor, except: [:created_at, :updated_at], status: :accepted
+    end
+
+    def destroy
+        instructor = finder
+        Instructor.destroy!(instructor)
+        head :no_content
+    end
+
+
+    
+    private
+        def finder
+            Instructor.find(params[:id])
+        end
+
+        def valid_params
+            params.permit(:name)
+        end
+
+        def not_found
+            render json: {error: "record not found"}, status: :not_found
+        end
+
+        def unprocessable_entity_response(invalid)
+            render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+        end
+
+end
